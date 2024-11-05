25c1100905e6795cc8ed7cdf70a7389d34371d40
Quang Huy <trinhquanghuy16092003@gmail.com>
2024-11-05 10:17:26 +0700

    hehe

---
+++ bai7.sql
@@ -0,0 +1,36 @@
+﻿﻿-- 1. Viết câu truy vấn để tạo bảng có tên DeleteTable gồm các thông tin sau: Mã sinh viên, Họ tên sinh viên, Phái, Ngày sinh, Nơi sinh, Tên khoa, Học bổng
+CREATE TABLE DeleteTable (
+    MaSV nvarchar(3) NOT NULL,
+    HoTenSV nvarchar(50) NOT NULL,
+    Phai bit NOT NULL,
+    NgaySinh smalldatetime NOT NULL,
+    NoiSinh nvarchar(100) NOT NULL,
+    TenKhoa nvarchar(50) NOT NULL,
+    HocBong float NULL
+);
+INSERT INTO DeleteTable (MaSV, HoTenSV, Phai, NgaySinh, NoiSinh, TenKhoa, HocBong)
+SELECT 
+    SV.MaSV, 
+    CONCAT(SV.HoSV, ' ', SV.TenSV) AS HoTenSV, 
+    SV.Phai, 
+    SV.NgaySinh, 
+    SV.NoiSinh, 
+    KH.TenKH AS TenKhoa, 
+    SV.HocBong
+FROM 
+    SinhVien SV
+JOIN 
+    Khoa KH ON SV.MaKH = KH.MaKH;
+-- 2. Xoá tất cả những sinh viên không có học bổng trong bảng DeleteTable
+DELETE FROM DeleteTable
+WHERE HocBong IS NULL OR HocBong = 0;
+-- 3. Xoá tất cả những sinh viên trong bảng DeleteTable sinh vào ngày 20/12/1987
+DELETE FROM DeleteTable
+WHERE NgaySinh = '1987-12-20';
+-- 4. Xoá tất cả những sinh viên trong bảng DeleteTable sinh trước tháng 3 năm 1987
+DELETE FROM DeleteTable
+WHERE NgaySinh < '1987-03-01';
+-- 5. Xoá tất cả những sinh viên nam của khoa Tin học trong bảng DeleteTable
+DELETE FROM DeleteTable
+WHERE Phai = 0 AND TenKhoa = 'Tin học';
+
---
+++ bai8.sql
@@ -0,0 +1,57 @@
+﻿-- 1. Cập nhật số tiết của môn Văn phạm thành 45 tiết
+UPDATE MonHoc
+SET Sotiet = 45
+WHERE TenMH = N'Văn phạm';
+
+-- 2. Cập nhật tên của sinh viên Trần Thanh Mai thành Trần Thanh Kỳ
+UPDATE SinhVien
+SET TenSV = N'Kỳ'
+WHERE HoSV = N'Trần Thanh' AND TenSV = N'Mai';
+
+-- 3. Cập nhật phái của sinh viên Trần Thanh Kỳ thành phái Nam
+UPDATE SinhVien
+SET Phai = 1
+WHERE HoSV = N'Trần Thanh' AND TenSV = N'Kỳ';
+
+-- 4. Cập nhật ngày sinh của sinh viên Trần Thị Thu Thủy thành ngày 05/07/1990
+UPDATE SinhVien
+SET NgaySinh = '1990-07-05'
+WHERE HoSV = N'Trần Thị Thu' AND TenSV = N'Thủy';
+
+-- 5. Tăng học bổng cho tất cả sinh viên của khoa Anh văn thêm 100,000
+UPDATE SinhVien
+SET HocBong = ISNULL(HocBong, 0) + 100000
+WHERE MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Anh văn');
+
+-- 6. Cộng thêm 5 điểm môn Trí Tuệ Nhân Tạo cho các sinh viên thuộc khoa Anh văn với điểm tối đa là 10
+UPDATE Ketqua
+SET Diem = CASE 
+             WHEN Diem + 5 > 10 THEN 10 
+             ELSE Diem + 5 
+           END
+WHERE MaMH = (SELECT MaMH FROM MonHoc WHERE TenMH = N'Trí Tuệ Nhân Tạo')
+  AND MaSV IN (SELECT MaSV FROM SinhVien WHERE MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Anh văn'));
+
+-- 7. Tăng học bổng cho sinh viên theo mô tả
+UPDATE SinhVien
+SET HocBong = ISNULL(HocBong, 0) + 
+              CASE 
+                WHEN Phai = 0 AND MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Anh văn') THEN 100000
+                WHEN Phai = 1 AND MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Tin học') THEN 150000
+                ELSE 50000
+              END;
+
+-- 8. Thay đổi kết quả thi của các sinh viên theo mô tả
+UPDATE Ketqua
+SET Diem = CASE 
+             WHEN MaSV IN (SELECT MaSV FROM SinhVien WHERE MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Anh văn'))
+                  AND Diem + 2 <= 10 THEN Diem + 2
+             WHEN MaSV IN (SELECT MaSV FROM SinhVien WHERE MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Anh văn'))
+                  AND Diem + 2 > 10 THEN 10
+             WHEN MaSV IN (SELECT MaSV FROM SinhVien WHERE MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Tin học'))
+                  AND Diem - 1 >= 0 THEN Diem - 1
+             WHEN MaSV IN (SELECT MaSV FROM SinhVien WHERE MaKH = (SELECT MaKH FROM Khoa WHERE TenKH = N'Tin học'))
+                  AND Diem - 1 < 0 THEN 0
+             ELSE Diem
+           END
+WHERE MaMH = (SELECT MaMH FROM MonHoc WHERE TenMH = N'Cơ sở dữ liệu');
